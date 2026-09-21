4cc485c96c69493e814e9ad5fa35cbe00dda500e
Aneesh kulkarni <aneeshkulkarni0920@gmail.com>
2026-09-22 02:22:20 +0530

    fix formatting of main.go with gofmt -w .

--- backend/cmd/kvstore/main.go
+++ backend/cmd/kvstore/main.go
@@ -7,29 +7,26 @@ import (
 	"github.com/aneesh1213/kvstore/backend/internals/store"
 )
 
-
-func main(){
+func main() {
 	fmt.Println("start of the kv store")
 
-	s := store.New();
+	s := store.New()
 
 	s.Set("name", "aneesh")
-    s.Set("language", "go")
+	s.Set("language", "go")
 
-	    v, err := s.Get("name")
-    if err != nil {
-        fmt.Println("unexpected error:", err)
-        return
-    }
-    fmt.Println("name =", v)
+	v, err := s.Get("name")
+	if err != nil {
+		fmt.Println("unexpected error:", err)
+		return
+	}
+	fmt.Println("name =", v)
 
-
-	_, err = s.Get("vikas");
-	if errors.Is(err, store.NotFound){
+	_, err = s.Get("vikas")
+	if errors.Is(err, store.NotFound) {
 		fmt.Println("missing key correctly reported as not found")
 	}
 
-	
-    s.Delete("language")
-    fmt.Println("keys after delete:", s.Len())
+	s.Delete("language")
+	fmt.Println("keys after delete:", s.Len())
 }
